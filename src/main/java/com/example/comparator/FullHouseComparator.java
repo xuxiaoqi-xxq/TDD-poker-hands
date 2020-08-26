4c5a23888d80c0810c5e1caa15e23ed375fcfb24
Eva Xu <xuev6@oocl.com>
2020-08-26 15:37:35 +0800

    [Henry & Eva] feature: implement full house comparator

---
+++ src/main/java/com/example/comparator/FullHouseComparator.java
@@ -0,0 +1,28 @@
+package com.example.comparator;
+
+import com.example.entity.PorkerHand;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+public class FullHouseComparator implements Comparator<PorkerHand> {
+
+    @Override
+    public int compare(PorkerHand porkerHand1, PorkerHand porkerHand2) {
+        List<Integer> faces1 = porkerHand1.getFaces();
+        List<Integer> faces2 = porkerHand2.getFaces();
+        HashSet<Integer> faceSet1 = new HashSet<>(faces1);
+        HashSet<Integer> faceSet2 = new HashSet<>(faces2);
+        boolean isFullHouse1 = faceSet1.size() == 2;
+        boolean isFullHouse2 = faceSet2.size() == 2;
+        if(isFullHouse1 && isFullHouse2) {
+            return faces1.get(2).compareTo(faces2.get(2));
+        }else if(isFullHouse1) {
+            return 1;
+        }else if(isFullHouse2){
+            return -1;
+        }
+        return 0;
+    }
+}
