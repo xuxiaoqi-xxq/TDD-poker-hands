9dc60a7e1d81d90e29183dac4a1957f47a2bd1a9
Eva Xu <xuev6@oocl.com>
2020-08-26 15:38:56 +0800

    [Henry & Eva] test: add full house comparator test

--- src/test/java/com/example/FullHouseComparatorTest.java
+++ src/test/java/com/example/FullHouseComparatorTest.java
@@ -23,4 +23,31 @@ public class FullHouseComparatorTest {
         //then
         assertEquals(1, result);
     }
+    @Test
+    void should_return_negative_1_when_call_compare_given_3H3C5D3S5S_and_4S5S4C4D5H() {
+        //given
+        String stringPorkerHand1 = "3H 3C 5D 3S 5S";
+        String stringPorkerHand2 = "4S 5S 4C 4D 5H";
+        PorkerHand porkerHand1 = PorkerHandUtil.parse(stringPorkerHand1);
+        PorkerHand porkerHand2 = PorkerHandUtil.parse(stringPorkerHand2);
+        FullHouseComparator fullHouseComparator = new FullHouseComparator();
+        //when
+        int result = fullHouseComparator.compare(porkerHand1, porkerHand2);
+        //then
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void should_return_0_when_call_compare_given_4S5S4C4D5H_and_4S5S4C4D5H() {
+        //given
+        String stringPorkerHand1 = "4S 5S 4C 4D 5H";
+        String stringPorkerHand2 = "4S 5S 4C 4D 5H";
+        PorkerHand porkerHand1 = PorkerHandUtil.parse(stringPorkerHand1);
+        PorkerHand porkerHand2 = PorkerHandUtil.parse(stringPorkerHand2);
+        FullHouseComparator fullHouseComparator = new FullHouseComparator();
+        //when
+        int result = fullHouseComparator.compare(porkerHand1, porkerHand2);
+        //then
+        assertEquals(0, result);
+    }
 }
