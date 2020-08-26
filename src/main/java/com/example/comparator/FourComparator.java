7a48ddcb203d6412ecf700f73f27447e4746b894
Eva Xu <xuev6@oocl.com>
2020-08-26 15:24:27 +0800

    [Henry & EVa] bug: fix four comparator bug

--- src/main/java/com/example/comparator/FourComparator.java
+++ src/main/java/com/example/comparator/FourComparator.java
@@ -3,24 +3,36 @@ package com.example.comparator;
 import com.example.entity.PorkerHand;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 
 public class FourComparator implements Comparator<PorkerHand> {
 
+    boolean isFour(List<Integer> faces){
+        int[] nums = new int[14];
+        for(int i = 0;i<faces.size();i++){
+            nums[faces.get(i)]++;
+        }
+        for (int num : nums) {
+            if (num == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public int compare(PorkerHand porkerHand1, PorkerHand porkerHand2) {
         List<Integer> faces1 = porkerHand1.getFaces();
         List<Integer> faces2 = porkerHand2.getFaces();
-        LinkedHashSet<Integer> set1 = new LinkedHashSet<>(faces1);
-        LinkedHashSet<Integer> set2 = new LinkedHashSet<>(faces2);
-        boolean isSameNumber1 = set1.size() == 2;
-        boolean isSameNumber2 = set2.size() == 2;
-        if (isSameNumber1 && isSameNumber2) {
+        boolean isFour1 = isFour(faces1);
+        boolean isFour2 = isFour(faces2);
+        if (isFour1 && isFour2) {
            return faces1.get(2).compareTo(faces2.get(2));
-        } else if (isSameNumber1) {
+        } else if (isFour1) {
             return 1;
-        } else if (isSameNumber2) {
+        } else if (isFour2) {
             return -1;
         }
         return 0;
