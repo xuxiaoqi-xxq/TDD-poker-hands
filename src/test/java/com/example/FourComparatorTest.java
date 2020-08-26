8bb6484c4515a0241e0dae9d31bba54c6a3c6117
Eva Xu <xuev6@oocl.com>
2020-08-26 15:13:27 +0800

    [Henry & EVa] test: add four comparator test

--- src/test/java/com/example/FourComparatorTest.java
+++ src/test/java/com/example/FourComparatorTest.java
@@ -22,4 +22,32 @@ public class FourComparatorTest {
         //then
         assertEquals(-1, result);
     }
+
+    @Test
+    void should_return_1_when_call_compare_given_7H3C7D7S7S_and_4S4S4C4D5H() {
+        //given
+        String stringPorkerHand1 = "7H 3C 7D 7S 7S";
+        String stringPorkerHand2 = "4S 4S 4C 4D 5H";
+        PorkerHand porkerHand1 = PorkerHandUtil.parse(stringPorkerHand1);
+        PorkerHand porkerHand2 = PorkerHandUtil.parse(stringPorkerHand2);
+        FourComparator fourComparator = new FourComparator();
+        //when
+        int result = fourComparator.compare(porkerHand1, porkerHand2);
+        //then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void should_return_0_when_call_compare_given_4S4S4C4D5H_and_4S4S4C4D5H() {
+        //given
+        String stringPorkerHand1 = "4S 4S 4C 4D 5H";
+        String stringPorkerHand2 = "4S 4S 4C 4D 5H";
+        PorkerHand porkerHand1 = PorkerHandUtil.parse(stringPorkerHand1);
+        PorkerHand porkerHand2 = PorkerHandUtil.parse(stringPorkerHand2);
+        FourComparator fourComparator = new FourComparator();
+        //when
+        int result = fourComparator.compare(porkerHand1, porkerHand2);
+        //then
+        assertEquals(0, result);
+    }
 }
