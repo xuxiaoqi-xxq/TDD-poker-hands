5e145ccd2ee9731062217f1bd961906deb0cc336
HUANGHE7 <huanghe7@oocl.com>
2020-08-27 17:08:59 +0800

    [henry & eva] feature: implement the main compare method

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -1,8 +1,29 @@
 package com.example;
 
-public class Demo {
+import com.example.comparator.*;
+import com.example.entity.PorkerHand;
+import com.example.util.PorkerHandUtil;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Demo {
+    private static final List<Comparator<PorkerHand>> comparatorList = Arrays.asList(
+            new StraightComparator(),
+            new FourComparator(),
+            new FullHouseComparator(),
+            new FlushComparator(),
+            new StraightComparator(),
+            new ThreeComparator(),
+            new TwoPairComparator()
+//            , new HighCardComparator()
+    );
     public String run() {
         return "ABC";
+    }
+
+    public int compare(String porkerHand1,String porkerHand2){
+        return comparatorList.stream().mapToInt(comparator -> comparator.compare(PorkerHandUtil.parse(porkerHand1), PorkerHandUtil.parse(porkerHand2))).filter(result -> result != 0).findFirst().orElse(0);
     }
 }
--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
     private static Demo demo;
 
     @BeforeAll
-    public static void setUp() throws Exception {
+    public static void setUp() {
         demo = new Demo();
     }
 
@@ -17,5 +18,22 @@ public class DemoTest {
     public void test01() {
         String result = demo.run();
         assertThat(result).isEqualTo("ABC");
+    }
+
+    @Test
+    void should_return_1_when_call_compare_given_2H4H3H9HKH_and_4H6C7DKSQS(){
+        String stringPorkerHand1 = "2H 4H 3H 9H KH";
+        String stringPorkerHand2 = "4H 6C 7D KS QS";
+        int compare = demo.compare(stringPorkerHand1, stringPorkerHand2);
+        assertEquals(1,compare);
+    }
+
+
+    @Test
+    void should_return_1_when_call_compare_given_3H4H5H6H7H_and_4H6C7DKSQS(){
+        String stringPorkerHand1 = "3H 4H 5H 6H 7H";
+        String stringPorkerHand2 = "4H 6C 7D KS QS";
+        int compare = demo.compare(stringPorkerHand1, stringPorkerHand2);
+        assertEquals(1,compare);
     }
 }
